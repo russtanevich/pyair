eee78a4d6c6410a97e77195f0c376e8ffe31b6fa
Ruslan <russtanevich@gmail.com>
2018-06-13 00:35:55 +0300

    add UI

--- src/webui.py
+++ src/webui.py
@@ -8,4 +8,7 @@ app = flask.Flask(__name__)
 @app.route("/")
 def hello_world():
     # return 'Hello, World!'
+    data = {
+        "own_planes": 1
+    }
     return flask.render_template('index.html', context={"a": 5, "b": 2})
